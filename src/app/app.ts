f44cc0e489d365cb4d6e094d0a5efa4e74c85022
reginald <lowbattery224@gmail.com>
2025-09-03 14:07:17 +0800

    removed the unnecesarry //

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,9 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: ./app.css'
 })
 export class App {
   protected readonly title = signal('CS305-web');
-  // 
 }
