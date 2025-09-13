6de213d907bab3f2b27ce55d895fd27ae873cac1
Kinley <kinleyjhonlavina01@gmail.com>
2025-09-13 19:44:45 +0800

    added SideBar by Kinley

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './footer/footer';
+import { SideBar } from './side-bar/side-bar';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer],
+  imports: [RouterOutlet, Footer,SideBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
