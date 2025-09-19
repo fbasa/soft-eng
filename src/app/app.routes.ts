a9c6199ef0758543172b477906807cdc21f6e2d5
Franklin Basa <basa.franklin@gmail.com>
2025-09-20 05:49:09 +0800

    updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,14 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
 
 export const routes: Routes = [
-    
+  {
+    path: '',
+    component: Dashboard,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
