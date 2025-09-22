8a98cc272f17b5681972253ecf7e62c5aa958436
chen <chenmaepadulla17@gmail.com>
2025-09-22 15:02:48 +0800

    Save my local changes

---
+++ src/app/pages/settings/settings.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-settings',
+  standalone: true,
+  templateUrl: './settings.html',
+  styleUrls: ['./settings.css']
+})
+export class SettingsComponent { }   // ✅ use SettingsComponent name
