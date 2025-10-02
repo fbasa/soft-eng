106c93f5c8a32f65c0a94ccc40b20aa4b773f13f
Kristel <kristelsheenreyes@gmail.com>
2025-10-02 22:41:06 +0800

    Added StudentUpdateComponent with redirect button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import {DashboardComponent } from './dashboard/dashboard';
 import { AddStudent } from './add-student/add-student';  
-
+import { StudentUpdateComponent } from './student-update/student-update';
 
 export const routes: Routes = [
     {path: 'dashboard', component: DashboardComponent},
     {path: 'add-student', component: AddStudent},
+      { path: 'student-update', component: StudentUpdateComponent } 
 ];
